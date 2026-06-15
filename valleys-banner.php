fd81b42a361a472a33a00382ce835e6a4fa0bda8
unknown <maged.ghabour2016@gmail.com>
2026-06-15 10:08:41 +0300

    elemntor widget

---
+++ valleys-banner.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name: Valleys Banner Elementor Widget
+ * Description: Custom Elementor Widget for the Valleys full-width banner.
+ * Plugin URI:  https://elementor.com/
+ * Version:     1.0.0
+ * Author:      Antigravity
+ * Author URI:  https://elementor.com/
+ * Text Domain: valleys-banner
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Register Valleys Banner Widget.
+ *
+ * Include widget file and register widget class.
+ *
+ * @since 1.0.0
+ * @param \Elementor\Widgets_Manager $widgets_manager Elementor widgets manager.
+ * @return void
+ */
+function register_valleys_banner_widget( $widgets_manager ) {
+
+	require_once( __DIR__ . '/widgets/banner-widget.php' );
+
+	$widgets_manager->register( new \Valleys_Banner_Widget() );
+
+}
+add_action( 'elementor/widgets/register', 'register_valleys_banner_widget' );
+
+/**
+ * Register widget scripts/styles.
+ */
+function register_valleys_banner_styles() {
+	wp_register_style( 'valleys-banner-style', plugins_url( 'assets/css/style.css', __FILE__ ) );
+}
+add_action( 'elementor/frontend/after_enqueue_styles', 'register_valleys_banner_styles' );
---
+++ widgets/banner-widget.php
@@ -0,0 +1,424 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+class Valleys_Banner_Widget extends \Elementor\Widget_Base {
+
+	public function get_name() {
+		return 'valleys_banner';
+	}
+
+	public function get_title() {
+		return esc_html__( 'Valleys Banner', 'valleys-banner' );
+	}
+
+	public function get_icon() {
+		return 'eicon-image-rollover';
+	}
+
+	public function get_categories() {
+		return [ 'general' ];
+	}
+
+	public function get_style_depends() {
+		return [ 'valleys-banner-style' ];
+	}
+
+	protected function register_controls() {
+
+		// -------------------------------------------------------------
+		// Content Tab
+		// -------------------------------------------------------------
+		$this->start_controls_section(
+			'content_section',
+			[
+				'label' => esc_html__( 'Content', 'valleys-banner' ),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'banner_image',
+			[
+				'label' => esc_html__( 'Banner Image', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::MEDIA,
+				'default' => [
+					'url' => \Elementor\Utils::get_placeholder_image_src(),
+				],
+			]
+		);
+
+		$this->add_control(
+			'right_text',
+			[
+				'label' => esc_html__( 'Right Text', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::TEXTAREA,
+				'default' => esc_html__( 'تنسيق يلفت الأنظار', 'valleys-banner' ),
+			]
+		);
+
+		$this->add_control(
+			'left_text',
+			[
+				'label' => esc_html__( 'Left Text', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::TEXTAREA,
+				'default' => esc_html__( 'ومذاق يليق بالمناسبات الراقية', 'valleys-banner' ),
+			]
+		);
+
+		$this->add_control(
+			'button_text',
+			[
+				'label' => esc_html__( 'Button Text', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'استكشف تشكيلتنا', 'valleys-banner' ),
+			]
+		);
+
+		$this->add_control(
+			'button_link',
+			[
+				'label' => esc_html__( 'Button Link', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::URL,
+				'placeholder' => esc_html__( 'https://your-link.com', 'valleys-banner' ),
+				'default' => [
+					'url' => '#',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+
+		// -------------------------------------------------------------
+		// Style Tab - Banner Settings
+		// -------------------------------------------------------------
+		$this->start_controls_section(
+			'style_image_section',
+			[
+				'label' => esc_html__( 'Banner Settings', 'valleys-banner' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_responsive_control(
+			'banner_height',
+			[
+				'label' => esc_html__( 'Banner Min Height', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%', 'vh' ],
+				'range' => [
+					'px' => [ 'min' => 200, 'max' => 1200 ],
+					'vh' => [ 'min' => 10, 'max' => 100 ],
+				],
+				'default' => [ 'unit' => 'px', 'size' => 500 ],
+				'selectors' => [
+					'{{WRAPPER}} .valleys-banner-container' => 'min-height: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'bg_position',
+			[
+				'label' => esc_html__( 'Background Position', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::SELECT,
+				'default' => 'center center',
+				'options' => [
+					'center center' => esc_html__( 'Center Center', 'valleys-banner' ),
+					'center top'    => esc_html__( 'Center Top', 'valleys-banner' ),
+					'center bottom' => esc_html__( 'Center Bottom', 'valleys-banner' ),
+					'left center'   => esc_html__( 'Left Center', 'valleys-banner' ),
+					'right center'  => esc_html__( 'Right Center', 'valleys-banner' ),
+				],
+				'selectors' => [
+					'{{WRAPPER}} .valleys-banner-container' => 'background-position: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+
+		// -------------------------------------------------------------
+		// Style Tab - Labels
+		// -------------------------------------------------------------
+		$this->start_controls_section(
+			'style_labels_section',
+			[
+				'label' => esc_html__( 'Labels Style', 'valleys-banner' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'label_color',
+			[
+				'label' => esc_html__( 'Text Color', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .valleys-label' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'label_bg_color',
+			[
+				'label' => esc_html__( 'Background Color', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .valleys-label' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'label_typography',
+				'selector' => '{{WRAPPER}} .valleys-label',
+			]
+		);
+
+		$this->add_responsive_control(
+			'labels_padding',
+			[
+				'label' => esc_html__( 'Labels Padding', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}} .valleys-label' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'labels_radius',
+			[
+				'label' => esc_html__( 'Labels Border Radius (Base)', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%' ],
+				'range' => [
+					'px' => [ 'min' => 0, 'max' => 100 ],
+				],
+				'selectors' => [
+					'{{WRAPPER}} .valleys-label.right' => 'border-radius: {{SIZE}}{{UNIT}} 0 0 {{SIZE}}{{UNIT}};',
+					'{{WRAPPER}} .valleys-label.left' => 'border-radius: 0 {{SIZE}}{{UNIT}} {{SIZE}}{{UNIT}} 0;',
+				],
+			]
+		);
+
+		$this->add_control(
+			'right_label_heading',
+			[
+				'label' => esc_html__( 'Right Label Positioning', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+		$this->add_responsive_control(
+			'right_label_position_top',
+			[
+				'label' => esc_html__( 'Vertical Position (%)', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ '%', 'px' ],
+				'range' => [ '%' => [ 'min' => 0, 'max' => 100 ] ],
+				'default' => [ 'unit' => '%', 'size' => 45 ],
+				'selectors' => [
+					'{{WRAPPER}} .valleys-label.right' => 'top: {{SIZE}}{{UNIT}}; transform: translateY(-50%);',
+				],
+			]
+		);
+		
+		$this->add_responsive_control(
+			'right_label_position_right',
+			[
+				'label' => esc_html__( 'Horizontal Position (px/%)', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%' ],
+				'range' => [ '%' => [ 'min' => -50, 'max' => 50 ], 'px' => [ 'min' => -200, 'max' => 200 ] ],
+				'default' => [ 'unit' => 'px', 'size' => 0 ],
+				'selectors' => [
+					'{{WRAPPER}} .valleys-label.right' => 'right: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'left_label_heading',
+			[
+				'label' => esc_html__( 'Left Label Positioning', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+		$this->add_responsive_control(
+			'left_label_position_top',
+			[
+				'label' => esc_html__( 'Vertical Position (%)', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ '%', 'px' ],
+				'range' => [ '%' => [ 'min' => 0, 'max' => 100 ] ],
+				'default' => [ 'unit' => '%', 'size' => 65 ],
+				'selectors' => [
+					'{{WRAPPER}} .valleys-label.left' => 'top: {{SIZE}}{{UNIT}}; transform: translateY(-50%);',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'left_label_position_left',
+			[
+				'label' => esc_html__( 'Horizontal Position (px/%)', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%' ],
+				'range' => [ '%' => [ 'min' => -50, 'max' => 50 ], 'px' => [ 'min' => -200, 'max' => 200 ] ],
+				'default' => [ 'unit' => 'px', 'size' => 0 ],
+				'selectors' => [
+					'{{WRAPPER}} .valleys-label.left' => 'left: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+
+		// -------------------------------------------------------------
+		// Style Tab - Button
+		// -------------------------------------------------------------
+		$this->start_controls_section(
+			'style_button_section',
+			[
+				'label' => esc_html__( 'Button Style', 'valleys-banner' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'button_color',
+			[
+				'label' => esc_html__( 'Text Color', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .valleys-btn' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'button_bg_color',
+			[
+				'label' => esc_html__( 'Background Color', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .valleys-btn' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'button_typography',
+				'selector' => '{{WRAPPER}} .valleys-btn',
+			]
+		);
+
+		$this->add_responsive_control(
+			'button_padding',
+			[
+				'label' => esc_html__( 'Button Padding', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}} .valleys-btn' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'button_border_radius',
+			[
+				'label' => esc_html__( 'Border Radius', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}} .valleys-btn' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+		
+		$this->add_responsive_control(
+			'button_bottom_position',
+			[
+				'label' => esc_html__( 'Button Bottom Position', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ '%', 'px' ],
+				'range' => [ '%' => [ 'min' => 0, 'max' => 100 ], 'px' => [ 'min' => 0, 'max' => 500 ] ],
+				'default' => [ 'unit' => '%', 'size' => 15 ],
+				'selectors' => [
+					'{{WRAPPER}} .valleys-btn-wrapper' => 'bottom: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'button_left_position',
+			[
+				'label' => esc_html__( 'Button Horizontal Position (%)', 'valleys-banner' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ '%' ],
+				'range' => [ '%' => [ 'min' => 0, 'max' => 100 ] ],
+				'default' => [ 'unit' => '%', 'size' => 50 ],
+				'selectors' => [
+					'{{WRAPPER}} .valleys-btn-wrapper' => 'left: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+	}
+
+	protected function render() {
+		$settings = $this->get_settings_for_display();
+		$image_url = $settings['banner_image']['url'];
+		
+		$this->add_render_attribute( 'wrapper', 'class', 'valleys-banner-container' );
+		
+		if ( ! empty( $image_url ) ) {
+			$this->add_render_attribute( 'wrapper', 'style', 'background-image: url(' . esc_url( $image_url ) . ');' );
+		}
+
+		if ( ! empty( $settings['button_link']['url'] ) ) {
+			$this->add_link_attributes( 'button', $settings['button_link'] );
+		}
+		$this->add_render_attribute( 'button', 'class', 'valleys-btn' );
+
+		?>
+		<div <?php $this->print_render_attribute_string( 'wrapper' ); ?>>
+			
+			<?php if ( ! empty( $settings['right_text'] ) ) : ?>
+				<div class="valleys-label right">
+					<?php echo nl2br( wp_kses_post( $settings['right_text'] ) ); ?>
+				</div>
+			<?php endif; ?>
+
+			<?php if ( ! empty( $settings['left_text'] ) ) : ?>
+				<div class="valleys-label left">
+					<?php echo nl2br( wp_kses_post( $settings['left_text'] ) ); ?>
+				</div>
+			<?php endif; ?>
+
+			<?php if ( ! empty( $settings['button_text'] ) ) : ?>
+				<div class="valleys-btn-wrapper">
+					<a <?php $this->print_render_attribute_string( 'button' ); ?>>
+						<?php echo esc_html( $settings['button_text'] ); ?>
+					</a>
+				</div>
+			<?php endif; ?>
+			
+		</div>
+		<?php
+	}
+}
